bc3d826a375ea47c8c303f02dcc858c06b2c0f88
Shaun Ruffell <sruffell@digium.com>
2013-07-25 15:57:16 -0500

    wip: listen.py remove some commented out lines

--- listen.py
+++ listen.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python
 
-#import socket
-#
-#s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-#s.bind(('hdlc0',0))
-##s.setsockopt(socket.SOL_SOCKET, 25, "hdlc0")
-#s.send(b'\000\001\002\003')
-##s.bind(("192.168.0.1", 8080))
-#while True:
-#	print s.recv(65565)
-
 import sys
 import pcap
 import array
